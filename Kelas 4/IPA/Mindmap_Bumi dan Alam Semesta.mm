6f6fd08f5bd1526b57b93f77ac03dc626cafd7e2
mazbrili <mazbrili@yahoo.com>
2025-04-10 15:07:43 +0700

    Add files via upload

---
+++ Kelas 4/IPA/Mindmap_Bumi dan Alam Semesta.mm
@@ -0,0 +1,24 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<map version="1.0.1">
+<!-- This file is saved using a hacked version of FreeMind. visit: http://freemind-mmx.sourceforge.net -->
+<!-- Orignal FreeMind, can download from http://freemind.sourceforge.net -->
+<!-- This .mm file is CVS/SVN friendly, some atts are saved in .mmx file. (from ossxp.com) -->
+<node ID="ID_642310254" 
+	TEXT="Bumi dan Alam Semesta">
+<icon BUILTIN="3-semesta"/>
+<node ID="ID_280712200" POSITION="right" 
+	TEXT="Permukaan Bumi: Terdiri dari daratan dan perairan."/>
+<node ID="ID_1803165238" POSITION="right" 
+	TEXT="Lapisan Bumi: (Pengenalan sederhana) Tanah, batuan."/>
+<node POSITION="right" 
+	TEXT="Kenampakan Alam: Gunung, sungai, danau, pantai, dataran tinggi, dataran rendah."/>
+<node POSITION="right" 
+	TEXT="Cuaca: Keadaan udara pada suatu tempat dan waktu tertentu (panas, dingin, berangin, hujan)."/>
+<node ID="ID_239896071" POSITION="left" 
+	TEXT="Bulan: Satelit alami Bumi."/>
+<node ID="ID_544242349" POSITION="left" 
+	TEXT="Tata Surya: (Pengenalan sederhana) Matahari sebagai pusat tata surya, planet-planet yang mengelilingi Matahari (Merkurius, Venus, Bumi, Mars, Jupiter, Saturnus, Uranus, Neptunus)."/>
+<node ID="ID_1352686886" POSITION="left" 
+	TEXT="Musim: Perubahan kondisi cuaca yang terjadi secara berulang dalam satu tahun (misalnya musim kemarau dan musim hujan di Indonesia)."/>
+</node>
+</map>
