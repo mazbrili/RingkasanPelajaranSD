fbe3892d997594e12570a4d6a51190ca5057693d
mazbrili <mazbrili@yahoo.com>
2025-04-10 12:59:10 +0700

    Bab Benda dan sifatnya

---
+++ Kelas 4/IPA/MindMap_BendaDanSifatnya.mm
@@ -0,0 +1,110 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<map version="1.0.1">
+<!-- This file is saved using a hacked version of FreeMind. visit: http://freemind-mmx.sourceforge.net -->
+<!-- Orignal FreeMind, can download from http://freemind.sourceforge.net -->
+<!-- This .mm file is CVS/SVN friendly, some atts are saved in .mmx file. (from ossxp.com) -->
+<node COLOR="#00b439" ID="ID_877860361" 
+	TEXT="Benda dan Sifatnya">
+<edge COLOR="#00b439" STYLE="bezier" WIDTH="2"/>
+<node COLOR="#990000" FOLDED="true" ID="ID_407880160" POSITION="right" 
+	TEXT="Jenis-jenis Benda">
+<edge COLOR="#990000" WIDTH="1"/>
+<node COLOR="#111111" FOLDED="true" ID="ID_566184432" 
+	TEXT="Berdasarkan Bentuk">
+<edge COLOR="#111111" WIDTH="thin"/>
+<node COLOR="#111111" ID="ID_357582276" 
+	TEXT="Padat">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+<node COLOR="#111111" ID="ID_649663669" 
+	TEXT="Cair">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+<node COLOR="#111111" ID="ID_1455204318" 
+	TEXT="Gas">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+</node>
+<node COLOR="#111111" FOLDED="true" ID="ID_711039802" 
+	TEXT="Berdasarkan Asal">
+<edge COLOR="#111111" WIDTH="thin"/>
+<node COLOR="#111111" ID="ID_1795722426" 
+	TEXT="Alam">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+<node COLOR="#111111" ID="ID_1191416137" 
+	TEXT="Buatan">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+</node>
+</node>
+<node COLOR="#990000" FOLDED="true" ID="ID_1070888696" POSITION="right" 
+	TEXT="Sifat-sifat Benda">
+<edge COLOR="#990000" WIDTH="1"/>
+<node COLOR="#111111" ID="ID_113224418" 
+	TEXT="Wujud dapat berubah">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+<node COLOR="#111111" ID="ID_22372470" 
+	TEXT="Mengalir atau tidak">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+<node COLOR="#111111" ID="ID_1976607759" 
+	TEXT="Mempunyai bentuk dan volume">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+<node COLOR="#111111" ID="ID_597451425" 
+	TEXT="Dapat kembali ke bentuk semula atau tidak (elastisitas)">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+</node>
+<node COLOR="#990000" FOLDED="true" HGAP="64" ID="ID_810866933" POSITION="left" 
+	TEXT="Perubahan Wujud Benda" VSHIFT="-24">
+<edge COLOR="#990000" WIDTH="1"/>
+<node COLOR="#111111" ID="ID_1937147446" 
+	TEXT="Mencair (padat → cair)">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+<node COLOR="#111111" ID="ID_822870474" 
+	TEXT="Membeku (cair → padat)">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+<node COLOR="#111111" ID="ID_869373000" 
+	TEXT="Menguap (cair → gas)">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+<node COLOR="#111111" ID="ID_403899731" 
+	TEXT="Mengembun (gas → cair)">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+<node COLOR="#111111" ID="ID_1878831765" 
+	TEXT="Menyublim (padat → gas)">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+<node COLOR="#111111" ID="ID_111106545" 
+	TEXT="Mengkristal (gas → padat)">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+</node>
+<node COLOR="#990000" FOLDED="true" ID="ID_11710065" POSITION="left" 
+	TEXT="Pemanfaatan Sifat Benda">
+<edge COLOR="#990000" WIDTH="1"/>
+<node COLOR="#111111" ID="ID_1844872159" 
+	TEXT="Benda elastis digunakan untuk karet gelang">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+<node COLOR="#111111" ID="ID_815862548" 
+	TEXT="Benda tahan panas digunakan untuk alat masak">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+<node COLOR="#111111" ID="ID_1708587717" 
+	TEXT="Benda kedap air digunakan untuk jas hujan">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+<node COLOR="#111111" ID="ID_1058162144" 
+	TEXT="Benda ringan digunakan untuk pesawat kertas">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+</node>
+</node>
+</map>
