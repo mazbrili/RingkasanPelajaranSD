2981988e336a320e05be740f08ae8b75f3567b8e
mazbrili <mazbrili@yahoo.com>
2025-04-10 13:03:49 +0700

    Add files via upload

---
+++ Kelas 4/IPA/MindMap_EnergiDanPerubahannya.mm
@@ -0,0 +1,84 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<map version="1.0.1">
+<!-- This file is saved using a hacked version of FreeMind. visit: http://freemind-mmx.sourceforge.net -->
+<!-- Orignal FreeMind, can download from http://freemind.sourceforge.net -->
+<!-- This .mm file is CVS/SVN friendly, some atts are saved in .mmx file. (from ossxp.com) -->
+<node ID="ID_308719537" 
+	TEXT="Energi dan Perubahannya">
+<icon BUILTIN="2-daun"/>
+<node FOLDED="true" ID="ID_1591147821" POSITION="right" 
+	TEXT="Pengertian Energi">
+<icon BUILTIN="3-tanya"/>
+<node 
+	TEXT="Kemampuan untuk melakukan usaha atau kerja"/>
+</node>
+<node FOLDED="true" ID="ID_914676232" POSITION="right" 
+	TEXT="Macam-macam Energi">
+<icon BUILTIN="3-warna-macam"/>
+<node 
+	TEXT="Energi panas (kalor)"/>
+<node 
+	TEXT="Energi cahaya"/>
+<node 
+	TEXT="Energi listrik"/>
+<node 
+	TEXT="Energi bunyi"/>
+<node 
+	TEXT="Energi gerak"/>
+<node 
+	TEXT="Energi kimia"/>
+</node>
+<node FOLDED="true" ID="ID_1679082921" POSITION="right" 
+	TEXT="Sumber Energi">
+<icon BUILTIN="03-detil"/>
+<node 
+	TEXT="Matahari"/>
+<node 
+	TEXT="Air"/>
+<node 
+	TEXT="Angin"/>
+<node 
+	TEXT="Batu bara"/>
+<node 
+	TEXT="Minyak bumi"/>
+<node 
+	TEXT="Makanan"/>
+</node>
+<node FOLDED="true" ID="ID_1325165179" POSITION="left" 
+	TEXT="Manfaat Energi">
+<icon BUILTIN="3-monitor"/>
+<node 
+	TEXT="Memasak makanan"/>
+<node 
+	TEXT="Menyalakan lampu"/>
+<node 
+	TEXT="Menggerakkan kendaraan"/>
+<node 
+	TEXT="Menghidupkan alat elektronik"/>
+</node>
+<node FOLDED="true" ID="ID_1484113898" POSITION="left" 
+	TEXT="Perubahan Energi">
+<icon BUILTIN="3-disuka"/>
+<node 
+	TEXT="Energi listrik → Energi panas (setrika)"/>
+<node 
+	TEXT="Energi listrik → Energi cahaya (lampu)"/>
+<node 
+	TEXT="Energi kimia → Energi gerak (baterai mainan)"/>
+<node 
+	TEXT="Energi gerak → Energi bunyi (gitar dipetik)"/>
+</node>
+<node FOLDED="true" ID="ID_485554048" POSITION="left" 
+	TEXT="Hemat Energi">
+<icon BUILTIN="3-data"/>
+<node 
+	TEXT="Mematikan lampu saat tidak digunakan"/>
+<node 
+	TEXT="Menggunakan alat hemat energi"/>
+<node 
+	TEXT="Memanfaatkan energi alternatif"/>
+<node 
+	TEXT="Tidak boros bahan bakar"/>
+</node>
+</node>
+</map>
