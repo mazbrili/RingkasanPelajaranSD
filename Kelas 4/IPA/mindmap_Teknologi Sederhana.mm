2cad001e558c159cdf6291d7c15b4f450a840f18
mazbrili <mazbrili@yahoo.com>
2025-04-10 15:13:55 +0700

    Add files via upload

---
+++ Kelas 4/IPA/mindmap_Teknologi Sederhana.mm
@@ -0,0 +1,62 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<map version="1.0.1">
+<!-- This file is saved using a hacked version of FreeMind. visit: http://freemind-mmx.sourceforge.net -->
+<!-- Orignal FreeMind, can download from http://freemind.sourceforge.net -->
+<!-- This .mm file is CVS/SVN friendly, some atts are saved in .mmx file. (from ossxp.com) -->
+<node ID="ID_1259783936" 
+	TEXT="Teknologi Sederhana">
+<icon BUILTIN="03-kimia"/>
+<node POSITION="right" 
+	TEXT="Pengertian: Alat/cara mempermudah kerja, prinsip IPA dasar, bahan mudah, cara sederhana."/>
+<node FOLDED="true" ID="ID_1753109733" POSITION="right" 
+	TEXT="Tujuan Mempelajari:">
+<node ID="ID_676367781" 
+	TEXT="Penerapan IPA"/>
+<node ID="ID_417182572" 
+	TEXT="Kembangkan keterampilan"/>
+<node ID="ID_1956919305" 
+	TEXT="Hargai karya"/>
+<node ID="ID_497551418" 
+	TEXT="Tumbuhkan minat sains"/>
+</node>
+<node FOLDED="true" ID="ID_547343217" POSITION="left" 
+	TEXT="Lainnya (Pengenalan):">
+<node ID="ID_1441788145" 
+	TEXT="Baji (contoh: kapak)"/>
+<node ID="ID_92446131" 
+	TEXT="Sekrup (contoh: baut)"/>
+</node>
+<node FOLDED="true" ID="ID_1664728148" POSITION="left" 
+	TEXT="Jenis &amp; Prinsip:">
+<node FOLDED="true" ID="ID_1785709382" 
+	TEXT="Prinsip: Momen gaya, titik tumpu">
+<node FOLDED="true" ID="ID_1392684448" 
+	TEXT="Tuas (Pengungkit):">
+<node ID="ID_1384928160" 
+	TEXT="Contoh: Gunting, linggis, jungkat-jungkit"/>
+</node>
+</node>
+<node FOLDED="true" ID="ID_632483867" 
+	TEXT="Roda Berporos:">
+<node ID="ID_1632920323" 
+	TEXT="Contoh: Gerobak, mobil-mobilan"/>
+<node ID="ID_1040150276" 
+	TEXT="Prinsip: Kurangi gesekan"/>
+</node>
+<node FOLDED="true" ID="ID_12262364" 
+	TEXT="Katrol:">
+<node ID="ID_1611455742" 
+	TEXT="Prinsip: Ubah arah gaya, kurangi gaya (majemuk)"/>
+<node ID="ID_1242099462" 
+	TEXT="Contoh: Timba sumur, kerekan bendera"/>
+</node>
+<node FOLDED="true" ID="ID_1396261259" 
+	TEXT="Bidang Miring:">
+<node ID="ID_1757411014" 
+	TEXT="Contoh: Tangga, papan landai"/>
+<node ID="ID_196212230" 
+	TEXT="Prinsip: Gaya kecil, jarak lebih jauh"/>
+</node>
+</node>
+</node>
+</map>
