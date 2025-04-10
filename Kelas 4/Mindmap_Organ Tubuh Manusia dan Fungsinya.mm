df5da1a2a14f8bbbfbf7ada6fbdecbc6e6fd508d
mazbrili <mazbrili@yahoo.com>
2025-04-10 16:57:01 +0700

    Add files via upload

---
+++ Kelas 4/Mindmap_Organ Tubuh Manusia dan Fungsinya.mm
@@ -0,0 +1,105 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<map version="1.0.1">
+<!-- This file is saved using a hacked version of FreeMind. visit: http://freemind-mmx.sourceforge.net -->
+<!-- Orignal FreeMind, can download from http://freemind.sourceforge.net -->
+<!-- This .mm file is CVS/SVN friendly, some atts are saved in .mmx file. (from ossxp.com) -->
+<node ID="ID_167263455" 
+	TEXT="Organ Tubuh Manusia dan Fungsinya:">
+<icon BUILTIN="pencil"/>
+<node FOLDED="true" HGAP="36" ID="ID_1155766989" POSITION="right" 
+	TEXT="Sistem Pencernaan: " VSHIFT="14">
+<node ID="ID_582980367" 
+	TEXT=" mencerna makanan menjadi zat gizi yang diserap tubuh.">
+<icon BUILTIN="idea"/>
+</node>
+<node 
+	TEXT="Mulut"/>
+<node 
+	TEXT="kerongkongan"/>
+<node 
+	TEXT="lambung"/>
+<node 
+	TEXT="usus halus"/>
+<node 
+	TEXT="usus besar"/>
+<node 
+	TEXT="anus"/>
+</node>
+<node FOLDED="true" ID="ID_1233249784" POSITION="right" 
+	TEXT="Sistem Pernapasan: ">
+<node 
+	TEXT="Hidung"/>
+<node 
+	TEXT="tenggorokan"/>
+<node 
+	TEXT="paru-paru"/>
+<node ID="ID_737547038" 
+	TEXT="mengambil oksigen dan mengeluarkan karbon dioksida.">
+<icon BUILTIN="idea"/>
+</node>
+</node>
+<node FOLDED="true" ID="ID_476175724" POSITION="right" 
+	TEXT="Sistem Peredaran Darah:">
+<node 
+	TEXT="Jantung"/>
+<node FOLDED="true" ID="ID_77447425" 
+	TEXT="pembuluh darah">
+<node 
+	TEXT="arteri"/>
+<node 
+	TEXT="vena"/>
+<node 
+	TEXT="kapiler"/>
+</node>
+<node 
+	TEXT="darah"/>
+<node ID="ID_1522681651" 
+	TEXT="Mengangkut oksigen, zat gizi, dan zat sisa ke seluruh tubuh.">
+<icon BUILTIN="idea"/>
+</node>
+</node>
+<node FOLDED="true" ID="ID_574954707" POSITION="left" 
+	TEXT="Sistem Ekskresi: ">
+<node 
+	TEXT="Ginjal"/>
+<node 
+	TEXT="ureter"/>
+<node 
+	TEXT="kandung kemih"/>
+<node 
+	TEXT="uretra"/>
+<node 
+	TEXT="kulit"/>
+<node 
+	TEXT="paru-paru"/>
+<node ID="ID_1586007238" 
+	TEXT="mengeluarkan zat sisa metabolisme dari tubuh.">
+<icon BUILTIN="idea"/>
+</node>
+</node>
+<node FOLDED="true" ID="ID_387321955" POSITION="left" 
+	TEXT="Alat Indera: ">
+<node 
+	TEXT="Mata (penglihatan)"/>
+<node 
+	TEXT="telinga (pendengaran)"/>
+<node 
+	TEXT="hidung (penciuman)"/>
+<node 
+	TEXT="lidah (perasa)"/>
+<node ID="ID_403184029" 
+	TEXT="kulit (peraba)"/>
+<node ID="ID_273786894" 
+	TEXT="menerima rangsangan dari lingkungan.">
+<icon BUILTIN="idea"/>
+</node>
+</node>
+<node FOLDED="true" ID="ID_1398338325" POSITION="left" 
+	TEXT="Rangka dan Otot: ">
+<node ID="ID_1674130955" 
+	TEXT="Rangka (tulang) : penopang tubuh dan pelindung organ vital."/>
+<node 
+	TEXT="Otot sebagai alat gerak aktif."/>
+</node>
+</node>
+</map>
