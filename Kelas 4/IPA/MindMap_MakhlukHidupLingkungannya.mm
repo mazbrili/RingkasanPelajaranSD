36691866fdcd3e3d4a9dbd5d62b050ff174704fc
mazbrili <mazbrili@yahoo.com>
2025-04-10 12:57:03 +0700

    Add files via upload

--- Kelas 4/IPA/MindMap_MakhlukHidupLingkungannya.mm
+++ Kelas 4/IPA/MindMap_MakhlukHidupLingkungannya.mm
@@ -1,2 +1,162 @@
-<?xml version='1.0' encoding='utf-8'?>
-<map version="0.9.0"><node TEXT="Makhluk Hidup dan Lingkungannya"><node TEXT="Ciri-ciri Makhluk Hidup"><node TEXT="Bernapas" /><node TEXT="Bergerak" /><node TEXT="Tumbuh dan berkembang" /><node TEXT="Memerlukan makanan" /><node TEXT="Berkembang biak" /><node TEXT="Peka terhadap rangsang" /><node TEXT="Mengeluarkan zat sisa" /></node><node TEXT="Kebutuhan Makhluk Hidup"><node TEXT="Makanan" /><node TEXT="Air" /><node TEXT="Tempat tinggal" /><node TEXT="Udara" /></node><node TEXT="Lingkungan Makhluk Hidup"><node TEXT="Komponen Biotik"><node TEXT="Manusia" /><node TEXT="Hewan" /><node TEXT="Tumbuhan" /></node><node TEXT="Komponen Abiotik"><node TEXT="Air" /><node TEXT="Udara" /><node TEXT="Tanah" /><node TEXT="Cahaya matahari" /><node TEXT="Suhu" /></node></node><node TEXT="Hubungan Antar Makhluk Hidup"><node TEXT="Simbiosis"><node TEXT="Mutualisme" /><node TEXT="Parasitisme" /><node TEXT="Komensalisme" /></node><node TEXT="Rantai Makanan"><node TEXT="Produsen" /><node TEXT="Konsumen (I, II, III)" /><node TEXT="Pengurai (dekomposer)" /></node></node><node TEXT="Pelestarian Lingkungan"><node TEXT="Menanam pohon" /><node TEXT="Tidak membuang sampah sembarangan" /><node TEXT="Menghemat air dan energi" /><node TEXT="Daur ulang" /></node></node></map>
+<?xml version="1.0" encoding="UTF-8"?>
+<map version="1.0.1">
+<!-- This file is saved using a hacked version of FreeMind. visit: http://freemind-mmx.sourceforge.net -->
+<!-- Orignal FreeMind, can download from http://freemind.sourceforge.net -->
+<!-- This .mm file is CVS/SVN friendly, some atts are saved in .mmx file. (from ossxp.com) -->
+<node COLOR="#111111" ID="ID_902009472" STYLE="fork" 
+	TEXT="Makhluk Hidup dan Lingkungannya">
+<edge COLOR="#111111" WIDTH="thin"/>
+<node COLOR="#111111" FOLDED="true" ID="ID_1809591630" POSITION="right" STYLE="fork" 
+	TEXT="Ciri-ciri Makhluk Hidup">
+<edge COLOR="#111111" WIDTH="thin"/>
+<node COLOR="#111111" ID="ID_455832091" STYLE="fork" 
+	TEXT="Bernapas">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+<node COLOR="#111111" ID="ID_236586165" STYLE="fork" 
+	TEXT="Bergerak">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+<node COLOR="#111111" ID="ID_1221023281" STYLE="fork" 
+	TEXT="Tumbuh dan berkembang">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+<node COLOR="#111111" ID="ID_1329789836" STYLE="fork" 
+	TEXT="Memerlukan makanan">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+<node COLOR="#111111" ID="ID_136573000" STYLE="fork" 
+	TEXT="Berkembang biak">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+<node COLOR="#111111" ID="ID_117168972" STYLE="fork" 
+	TEXT="Peka terhadap rangsang">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+<node COLOR="#111111" ID="ID_563126373" STYLE="fork" 
+	TEXT="Mengeluarkan zat sisa">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+</node>
+<node COLOR="#111111" FOLDED="true" ID="ID_942687838" POSITION="right" STYLE="fork" 
+	TEXT="Kebutuhan Makhluk Hidup">
+<edge COLOR="#111111" WIDTH="thin"/>
+<node COLOR="#111111" ID="ID_176440612" STYLE="fork" 
+	TEXT="Makanan">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+<node COLOR="#111111" ID="ID_1309679345" STYLE="fork" 
+	TEXT="Air">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+<node COLOR="#111111" ID="ID_488899548" STYLE="fork" 
+	TEXT="Tempat tinggal">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+<node COLOR="#111111" ID="ID_1289616989" STYLE="fork" 
+	TEXT="Udara">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+</node>
+<node COLOR="#111111" FOLDED="true" ID="ID_1619217145" POSITION="right" STYLE="fork" 
+	TEXT="Lingkungan Makhluk Hidup">
+<edge COLOR="#111111" WIDTH="thin"/>
+<node COLOR="#111111" FOLDED="true" ID="ID_403044655" STYLE="fork" 
+	TEXT="Komponen Biotik">
+<edge COLOR="#111111" WIDTH="thin"/>
+<node COLOR="#111111" ID="ID_534895361" STYLE="fork" 
+	TEXT="Manusia">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+<node COLOR="#111111" ID="ID_1305347271" STYLE="fork" 
+	TEXT="Hewan">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+<node COLOR="#111111" ID="ID_153507527" STYLE="fork" 
+	TEXT="Tumbuhan">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+</node>
+<node COLOR="#111111" FOLDED="true" ID="ID_551816130" STYLE="fork" 
+	TEXT="Komponen Abiotik">
+<edge COLOR="#111111" WIDTH="thin"/>
+<node COLOR="#111111" ID="ID_768662562" STYLE="fork" 
+	TEXT="Air">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+<node COLOR="#111111" ID="ID_366665007" STYLE="fork" 
+	TEXT="Udara">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+<node COLOR="#111111" ID="ID_1862898620" STYLE="fork" 
+	TEXT="Tanah">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+<node COLOR="#111111" ID="ID_831027007" STYLE="fork" 
+	TEXT="Cahaya matahari">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+<node COLOR="#111111" ID="ID_413532789" STYLE="fork" 
+	TEXT="Suhu">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+</node>
+</node>
+<node COLOR="#111111" FOLDED="true" ID="ID_283944650" POSITION="left" STYLE="fork" 
+	TEXT="Hubungan Antar Makhluk Hidup">
+<edge COLOR="#111111" WIDTH="thin"/>
+<node COLOR="#111111" FOLDED="true" ID="ID_1721141815" STYLE="fork" 
+	TEXT="Simbiosis">
+<edge COLOR="#111111" WIDTH="thin"/>
+<node COLOR="#111111" ID="ID_1674450743" STYLE="fork" 
+	TEXT="Mutualisme">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+<node COLOR="#111111" ID="ID_1905956491" STYLE="fork" 
+	TEXT="Parasitisme">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+<node COLOR="#111111" ID="ID_340425229" STYLE="fork" 
+	TEXT="Komensalisme">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+</node>
+<node COLOR="#111111" FOLDED="true" ID="ID_1936587252" STYLE="fork" 
+	TEXT="Rantai Makanan">
+<edge COLOR="#111111" WIDTH="thin"/>
+<node COLOR="#111111" ID="ID_1282560153" STYLE="fork" 
+	TEXT="Produsen">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+<node COLOR="#111111" ID="ID_1710959737" STYLE="fork" 
+	TEXT="Konsumen (I, II, III)">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+<node COLOR="#111111" ID="ID_1787291430" STYLE="fork" 
+	TEXT="Pengurai (dekomposer)">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+</node>
+</node>
+<node COLOR="#111111" FOLDED="true" ID="ID_1982555857" POSITION="left" STYLE="fork" 
+	TEXT="Pelestarian Lingkungan">
+<edge COLOR="#111111" WIDTH="thin"/>
+<node COLOR="#111111" ID="ID_411132942" STYLE="fork" 
+	TEXT="Menanam pohon">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+<node COLOR="#111111" ID="ID_1703972808" STYLE="fork" 
+	TEXT="Tidak membuang sampah sembarangan">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+<node COLOR="#111111" ID="ID_168335620" STYLE="fork" 
+	TEXT="Menghemat air dan energi">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+<node COLOR="#111111" ID="ID_1038780273" STYLE="fork" 
+	TEXT="Daur ulang">
+<edge COLOR="#111111" WIDTH="thin"/>
+</node>
+</node>
+</node>
+</map>
