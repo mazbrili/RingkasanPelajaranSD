a61e330c7d3de5725351053c6eb46a0172753149
mazbrili <mazbrili@yahoo.com>
2025-04-16 16:57:24 +0700

    Add files via upload

---
+++ Matematika kelas 5.mm
@@ -0,0 +1,79 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<map version="1.0.1">
+<!-- This file is saved using a hacked version of FreeMind. visit: http://freemind-mmx.sourceforge.net -->
+<!-- Orignal FreeMind, can download from http://freemind.sourceforge.net -->
+<!-- This .mm file is CVS/SVN friendly, some atts are saved in .mmx file. (from ossxp.com) -->
+<node ID="ID_321029500" 
+	TEXT="Matematika kelas 5">
+<node FOLDED="true" ID="ID_248078142" POSITION="right" 
+	TEXT="Bilangan Bulat:">
+<node ID="ID_511785666" 
+	TEXT="Sifat-sifat operasi hitung (komutatif, assosiatif, distributif)"/>
+<node ID="ID_1316925728" 
+	TEXT="Urutan operasi hitung"/>
+<node ID="ID_655684915" 
+	TEXT="Operasi hitung bilangan bulat (penjumlahan, pengurangan, perkalian, pembagian) yang lebih kompleks, termasuk bilangan negatif yang lebih besar."/>
+</node>
+<node FOLDED="true" ID="ID_1087963947" POSITION="right" 
+	TEXT="Bilangan Desimal:">
+<node ID="ID_1461791733" 
+	TEXT="Nilai tempat pada bilangan desimal"/>
+<node ID="ID_477313204" 
+	TEXT="Operasi hitung bilangan desimal (penjumlahan, pengurangan, perkalian, pembagian)"/>
+<node ID="ID_726145018" 
+	TEXT="Mengubah pecahan biasa menjadi desimal dan sebaliknya"/>
+</node>
+<node FOLDED="true" ID="ID_38575286" POSITION="right" 
+	TEXT="Pecahan:">
+<node ID="ID_1970595410" 
+	TEXT="Operasi hitung pecahan campuran"/>
+<node ID="ID_1203782747" 
+	TEXT="Mengurutkan pecahan"/>
+<node ID="ID_161064193" 
+	TEXT="Menyelesaikan masalah sehari-hari yang melibatkan pecahan"/>
+</node>
+<node FOLDED="true" ID="ID_1861621656" POSITION="right" 
+	TEXT="Pengukuran:">
+<node ID="ID_1182632805" 
+	TEXT="Mengukur panjang, berat, waktu, dan suhu"/>
+<node ID="ID_1608213254" 
+	TEXT="Mengubah satuan panjang, berat, dan waktu yang lebih kompleks"/>
+</node>
+<node FOLDED="true" ID="ID_1079550679" POSITION="right" 
+	TEXT="Bangun Datar:">
+<node ID="ID_1214521720" 
+	TEXT="Luas dan keliling berbagai bangun datar (jajar genjang, trapesium, layang-layang)"/>
+<node ID="ID_666595294" 
+	TEXT="Sifat-sifat bangun datar (simetri, kongruen)"/>
+</node>
+<node FOLDED="true" ID="ID_314717643" POSITION="left" 
+	TEXT="Bangun Ruang:">
+<node ID="ID_280008722" 
+	TEXT="Jaring-jaring bangun ruang"/>
+<node ID="ID_852617282" 
+	TEXT="Luas permukaan dan volume kubus, balok, dan prisma"/>
+</node>
+<node FOLDED="true" ID="ID_1879721400" POSITION="left" 
+	TEXT="Data dan Grafik:">
+<node ID="ID_552579279" 
+	TEXT="Menyajikan data dalam bentuk diagram batang, diagram garis, diagram lingkaran, dan piktogram"/>
+<node ID="ID_1649373187" 
+	TEXT="Menganalisis data dari berbagai jenis diagram"/>
+</node>
+<node FOLDED="true" ID="ID_1373801635" POSITION="left" 
+	TEXT="Persentase:">
+<node ID="ID_195409106" 
+	TEXT="Konsep persentase"/>
+</node>
+<node FOLDED="true" ID="ID_1323860150" POSITION="left" 
+	TEXT="Menentukan persentase suatu bilangan">
+<node ID="ID_489225779" 
+	TEXT="Menyelesaikan masalah yang berkaitan dengan persentase (diskon, keuntungan, kerugian)"/>
+</node>
+<node FOLDED="true" ID="ID_1812508510" POSITION="left" 
+	TEXT="Aritmetika Sosial:">
+<node ID="ID_381671378" 
+	TEXT="Menyelesaikan masalah sehari-hari yang berkaitan dengan uang, bunga tunggal, dan persentase"/>
+</node>
+</node>
+</map>
